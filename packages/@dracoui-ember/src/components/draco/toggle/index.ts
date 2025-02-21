47438f0421990d83801f96b6441a5a04d7683206
botond-szabo <112097901+botond-szabo@users.noreply.github.com>
2025-02-21 13:51:50 -0700

    Added new toggle component

--- packages/@dracoui-ember/src/components/draco/toggle/index.ts
+++ packages/@dracoui-ember/src/components/draco/toggle/index.ts
@@ -1,22 +1,29 @@
 import {
+  DracoToggleSizeValues,
   DracoToggleColorValues
 } from "./types.ts";
 import { assert } from "@ember/debug";
 import Component from "@glimmer/component";
 
+import type {
+  DracoToggleSizes,
+  DracoToggleColors
+} from "./types";
 import type { DracoIconSignature } from "../icon";
-import type { DracoToggleColors } from "./types";
 
 export const DEFAULT_CHECKED = false;
 export const DEFAULT_DISABLED = false;
+export const DEFAULT_SIZE = DracoToggleSizeValues.Medium;
 export const DEFAULT_COLOR = DracoToggleColorValues.Primary;
 
+export const AVAILABLE_SIZES: string[] = Object.values(DracoToggleSizeValues);
 export const AVAILABLE_COLORS: string[] = Object.values(DracoToggleColorValues);
 
 export interface DracoToggleSignature {
   Args: {
     checked?: boolean;
     disabled?: boolean;
+    size?: DracoToggleSizes;
     color?: DracoToggleColors;
     text?: string | undefined;
     icon?: DracoIconSignature['Args']['name'];
@@ -55,7 +62,7 @@ export default class DracoToggle extends Component<DracoToggleSignature> {
     const { color = DEFAULT_COLOR } = this.args;
 
     assert(
-      `@color for "Draco::toggle" must be one of the following: ${AVAILABLE_COLORS.join(
+      `@color for "Draco::Toggle" must be one of the following: ${AVAILABLE_COLORS.join(
         ', '
       )}; received: ${color}`,
       AVAILABLE_COLORS.includes(color)
@@ -64,11 +71,32 @@ export default class DracoToggle extends Component<DracoToggleSignature> {
     return color;
   }
 
+  get size(): DracoToggleSizes {
+    const { size = DEFAULT_SIZE } = this.args;
+
+    assert(
+      `@size for "Draco::Toggle" must be one of the following: ${AVAILABLE_SIZES.join(
+        ', '
+      )}; received: ${size}`,
+      AVAILABLE_SIZES.includes(size)
+    );
+
+    return size;
+  }
+
   get classNames(): string {
-    const classes = ['draco-toggle'];
+    /**
+     * @note
+     * The default class goes to the indicator element instead
+     * of the parent element as in all other components.
+    */
+    const classes = ['draco-toggle__thumb'];
 
     // add color classes
-    classes.push(`draco-toggle--color-${this.color}`)
+    classes.push(`draco-toggle--color-${this.color}`);
+
+    // add the size classes
+    classes.push(`draco-toggle--size-${this.size}`);
 
     return classes.join(' ');
   }
