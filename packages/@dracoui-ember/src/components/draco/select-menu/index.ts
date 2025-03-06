fb61e5ce200525b451db3dfc948c89134a696e13
Botond Szabo <112097901+botond-szabo@users.noreply.github.com>
2025-03-05 23:06:38 -0700

    Added new components page and started working on dynamic sidebar

---
+++ packages/@dracoui-ember/src/components/draco/select-menu/index.ts
@@ -0,0 +1,11 @@
+import Component from "@glimmer/component";
+
+export interface DracoSelectMenuSignature {
+  Args: {
+
+  };
+}
+
+export default class DracoSelectMenu extends Component<DracoSelectMenuSignature> {
+
+};
